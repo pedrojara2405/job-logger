eb7396a7941b368ca63a5083c7568f42e6a70543
Pedro Jara <pedrojara2405@gmail.com>
2018-10-23 00:55:33 -0500

    Updating RecluterUserController

---
+++ JobLogger.Backend/JobLogger.Api/Controllers/RecluterUserController.cs
@@ -0,0 +1,33 @@
+﻿using JobLogger.Business;
+using JobLogger.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace JobLogger.Api.Controllers
+{
+    public class RecluterUserController : ApiController
+    {
+        private RecluterUserBusiness _recluterUserBusiness;
+
+        public RecluterUserController()
+        {
+            _recluterUserBusiness = new RecluterUserBusiness();
+        }
+
+        public IHttpActionResult GetAll()
+        {
+            List<RecluterUser> recluterUsers = _recluterUserBusiness.GetAll().ToList();
+            return Ok(200);
+        }
+
+        public IHttpActionResult Get(int recluterUserId)
+        {
+            RecluterUser recluterUser = _recluterUserBusiness.Get(recluterUserId);
+            return Ok(200);
+        }
+    }
+}
